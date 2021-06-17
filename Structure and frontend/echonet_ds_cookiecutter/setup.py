9e48bdc784287405f9d77a962e5af8466c13b501
muditsinha01 <55638747+muditsinha01@users.noreply.github.com>
2021-06-17 21:22:24 +0530

    Add files via upload

---
+++ Structure and frontend/echonet_ds_cookiecutter/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model which takes in a DICOM file and classifies the image and returns an ejection fraction',
+    author='Mudit Sinha, Amruth Jaligma, Eeshan Gandhi',
+    license='',
+)
